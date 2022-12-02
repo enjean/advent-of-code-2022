a810f152e39b52f11bc99e913cbd12cdd28804ae
Eric Jean <eric.jean@wwt.com>
2022-12-02 15:57:06 -0700

    Day 1 Part 2

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -8,6 +8,11 @@ fun findMaxCarried(caloriesCarried: List<List<Int>>) : Int =
 fun main() {
     val inputLines = ParseUtil.inputLines(1)
     val inputAsInts = ParseUtil.parseGroups(inputLines).map { it.map { str -> str.toInt() } }
+    val sums = inputAsInts.map { it.sum() }
+    val sortedSums = sums.sorted().reversed()
 
-    println("Part 1: ${findMaxCarried(inputAsInts)}")
+    println("Part 1: ${sortedSums.first()}")
+
+    val part2 = sortedSums[0] + sortedSums[1] + sortedSums[2]
+    println("Part 2: $part2")
 }
