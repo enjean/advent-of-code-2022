892942d131754408df6552772e0a3f4c1af3ca35
Eric Jean <eric.jean@wwt.com>
2023-01-13 15:39:56 -0700

    Day 2 Part 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -13,6 +13,7 @@ private val shapeBeaters = mapOf(
     Shape.SCISSORS to Shape.PAPER,
     Shape.PAPER to Shape.ROCK,
 )
+private val shapeLosers = shapeBeaters.entries.associate { it.value to it.key }
 
 data class Round(
     val opponentSelection: Shape,
@@ -27,13 +28,8 @@ enum class Result(val score: Int) {
 
 fun parseRound(line: String): Round {
     val moves = line.split(" ")
-    val opponentSelection = when(moves[0]) {
-        "A" -> Shape.ROCK
-        "B" -> Shape.PAPER
-        "C" -> Shape.SCISSORS
-        else -> throw IllegalArgumentException("Unexpected ${moves[0]}")
-    }
-    val mySelection = when(moves[1]) {
+    val opponentSelection = parseOpponentSelection(moves[0])
+    val mySelection = when (moves[1]) {
         "X" -> Shape.ROCK
         "Y" -> Shape.PAPER
         "Z" -> Shape.SCISSORS
@@ -41,6 +37,26 @@ fun parseRound(line: String): Round {
     }
     return Round(opponentSelection, mySelection)
 }
+
+fun parseRound2(line: String): Round2 {
+    val lineParts = line.split(" ")
+    val opponentSelection = parseOpponentSelection(lineParts[0])
+    val desiredResult = when (lineParts[1]) {
+        "X" -> Result.LOSS
+        "Y" -> Result.DRAW
+        "Z" -> Result.WIN
+        else -> throw IllegalArgumentException("Unexpected $lineParts[1]")
+    }
+    return Round2(opponentSelection, desiredResult)
+}
+
+private fun parseOpponentSelection(move: String) =
+    when (move) {
+        "A" -> Shape.ROCK
+        "B" -> Shape.PAPER
+        "C" -> Shape.SCISSORS
+        else -> throw IllegalArgumentException("Unexpected $move")
+    }
 
 fun findResult(round: Round): Result =
     when {
@@ -52,9 +68,30 @@ fun findResult(round: Round): Result =
 fun scoreRound(round: Round): Int =
     round.mySelection.value + findResult(round).score
 
+data class Round2(
+    val opponentSelection: Shape,
+    val desiredResult: Result,
+)
+
+fun findMySelection(round2: Round2): Shape =
+    when (round2.desiredResult) {
+        Result.DRAW -> round2.opponentSelection
+        Result.LOSS -> checkNotNull(shapeBeaters[round2.opponentSelection])
+        Result.WIN -> checkNotNull(shapeLosers[round2.opponentSelection])
+    }
+
+fun scoreRound(round2: Round2): Int =
+    findMySelection(round2).value + round2.desiredResult.score
+
+
 fun main() {
-    val strategyGuide = ParseUtil.inputLines(2).map(::parseRound)
+    val inputLines = ParseUtil.inputLines(2)
+    val strategyGuide = inputLines.map(::parseRound)
 
     val score = strategyGuide.sumOf(::scoreRound)
     println("Part 1 = $score")
+
+    val part2Guide = inputLines.map(::parseRound2)
+    val part2Score = part2Guide.sumOf(::scoreRound)
+    println("Part 2 = $part2Score")
 }
--- src/test/kotlin/day2/Day2KtTest.kt
+++ src/test/kotlin/day2/Day2KtTest.kt
@@ -1,6 +1,7 @@
 package day2
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -9,16 +10,49 @@ import org.junit.jupiter.params.provider.ArgumentsSource
 import java.util.stream.Stream
 
 class Day2KtTest {
-    @ParameterizedTest
-    @ArgumentsSource(FindResultArgumentsProvider::class)
-    fun `find result test`(
-        round: Round,
-        expectedResult: Result
-    ) {
-        assertEquals(expectedResult, findResult(round))
+    @Nested
+    inner class Part1 {
+        @ParameterizedTest
+        @ArgumentsSource(FindResultArgumentsProvider::class)
+        fun `find result test`(
+            round: Round,
+            expectedResult: Result
+        ) {
+            assertEquals(expectedResult, findResult(round))
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(ScoreRoundTestArgumentsProvider::class)
+        fun `score round test`(
+            round: Round,
+            expectedScore: Int
+        ) {
+            assertEquals(expectedScore, scoreRound(round))
+        }
     }
 
-    class FindResultArgumentsProvider: ArgumentsProvider {
+    @Nested
+    inner class Part2 {
+        @ParameterizedTest
+        @ArgumentsSource(value = FindMySelectionArgumentsProvider::class)
+        fun `find my selection test`(
+            round2: Round2,
+            expectedShape: Shape,
+        ) {
+            assertEquals(expectedShape, findMySelection(round2))
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(value = ScoreRound2TestArgumentsProvider::class)
+        fun `score round test`(
+            round2: Round2,
+            expectedScore: Int,
+        ) {
+            assertEquals(expectedScore, scoreRound(round2))
+        }
+    }
+
+    class FindResultArgumentsProvider : ArgumentsProvider {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
             Stream.of(
                 Arguments.of(Round(opponentSelection = Shape.ROCK, mySelection = Shape.PAPER), Result.WIN),
@@ -33,21 +67,55 @@ class Day2KtTest {
             )
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(ScoreRoundTestArgumentsProvider::class)
-    fun `score round test`(
-        round: Round,
-        expectedScore: Int
-    ) {
-        assertEquals(expectedScore, scoreRound(round))
-    }
-
-    class ScoreRoundTestArgumentsProvider: ArgumentsProvider {
+    class ScoreRoundTestArgumentsProvider : ArgumentsProvider {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
             Stream.of(
-                Arguments.of(Round(opponentSelection = Shape.ROCK, mySelection = Shape.PAPER), 8),   // Paper(2) + Win(6)
-                Arguments.of(Round(opponentSelection = Shape.PAPER, mySelection = Shape.ROCK), 1),   // Rock(1) + Loss(0)
-                Arguments.of(Round(opponentSelection = Shape.SCISSORS, mySelection = Shape.SCISSORS), 6), // Scissors(3) + Draw (3)
+                Arguments.of(
+                    Round(opponentSelection = Shape.ROCK, mySelection = Shape.PAPER),
+                    8
+                ),   // Paper(2) + Win(6)
+                Arguments.of(
+                    Round(opponentSelection = Shape.PAPER, mySelection = Shape.ROCK),
+                    1
+                ),   // Rock(1) + Loss(0)
+                Arguments.of(
+                    Round(opponentSelection = Shape.SCISSORS, mySelection = Shape.SCISSORS),
+                    6
+                ), // Scissors(3) + Draw (3)
+            )
+
+    }
+
+    class FindMySelectionArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
+            Stream.of(
+                Arguments.of(Round2(opponentSelection = Shape.ROCK, desiredResult = Result.WIN), Shape.PAPER),
+                Arguments.of(Round2(opponentSelection = Shape.ROCK, desiredResult = Result.LOSS), Shape.SCISSORS),
+                Arguments.of(Round2(opponentSelection = Shape.ROCK, desiredResult = Result.DRAW), Shape.ROCK),
+                Arguments.of(Round2(opponentSelection = Shape.PAPER, desiredResult = Result.WIN), Shape.SCISSORS),
+                Arguments.of(Round2(opponentSelection = Shape.PAPER, desiredResult = Result.LOSS), Shape.ROCK),
+                Arguments.of(Round2(opponentSelection = Shape.PAPER, desiredResult = Result.DRAW), Shape.PAPER),
+                Arguments.of(Round2(opponentSelection = Shape.SCISSORS, desiredResult = Result.WIN), Shape.ROCK),
+                Arguments.of(Round2(opponentSelection = Shape.SCISSORS, desiredResult = Result.LOSS), Shape.PAPER),
+                Arguments.of(Round2(opponentSelection = Shape.SCISSORS, desiredResult = Result.DRAW), Shape.SCISSORS),
+            )
+    }
+
+    class ScoreRound2TestArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
+            Stream.of(
+                Arguments.of(
+                    Round2(opponentSelection = Shape.ROCK, desiredResult = Result.DRAW),
+                    4
+                ),   // Rock(1) + Draw(3)
+                Arguments.of(
+                    Round2(opponentSelection = Shape.PAPER, desiredResult = Result.LOSS),
+                    1
+                ),   // Rock(1) + Loss(0)
+                Arguments.of(
+                    Round2(opponentSelection = Shape.SCISSORS, desiredResult = Result.WIN),
+                    7
+                ), // Rock(1) + Win (6)
             )
 
     }
