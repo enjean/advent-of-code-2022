b177263a37749c3534e2763f659b671fc750a3ae
Eric Jean <eric.jean@wwt.com>
2023-01-31 15:38:40 -0700

    Day 6 Part 2

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -3,23 +3,26 @@ package day6
 import util.ParseUtil
 import java.util.*
 
-fun charactersBeforeMarker(input: String): Int {
+fun findDistinctCharacters(input: String, numDistinct: Int): Int {
     val charactersQueue: Queue<Char> = LinkedList()
     input.forEachIndexed { index, c ->
-        if (charactersQueue.size == 4) {
+        if (charactersQueue.size == numDistinct) {
             charactersQueue.remove()
         }
         charactersQueue.add(c)
-        if (charactersQueue.distinct().size == 4) {
+        if (charactersQueue.distinct().size == numDistinct) {
             return index + 1
         }
     }
-    throw IllegalArgumentException("Marker not found")
+    throw IllegalArgumentException("Condition not found")
 }
 
 fun main() {
     val input = ParseUtil.inputLines(6).single()
 
-    val part1 = charactersBeforeMarker(input)
+    val part1 = findDistinctCharacters(input, 4)
     println("Part 1 = $part1")
+
+    val part2 = findDistinctCharacters(input, 14)
+    println("Part 2 = $part2")
 }
--- src/test/kotlin/day6/Day6KtTest.kt
+++ src/test/kotlin/day6/Day6KtTest.kt
@@ -10,22 +10,27 @@ import java.util.stream.Stream
 
 class Day6KtTest {
     @ParameterizedTest
-    @ArgumentsSource(value = CharactersBeforeMarkerArgumentsProvider::class)
-    fun `characters before marker`(
-        input: String,
+    @ArgumentsSource(value = FindDistinctCharactersArgumentsProvider::class)
+    fun `find distinct characters`(
+        input: String, numDistinct: Int,
         expectedNumberOfCharactersBeforeMarker: Int,
     ){
-        assertEquals(expectedNumberOfCharactersBeforeMarker, charactersBeforeMarker(input))
+        assertEquals(expectedNumberOfCharactersBeforeMarker, findDistinctCharacters(input, numDistinct))
     }
 
-    private class CharactersBeforeMarkerArgumentsProvider : ArgumentsProvider {
+    private class FindDistinctCharactersArgumentsProvider : ArgumentsProvider {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> =
             Stream.of(
-                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 6),
-                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4, 7),
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5),
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 4, 6),
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4, 10),
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11),
+                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14, 19),
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 14, 23),
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 14, 23),
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14, 29),
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14, 26),
             )
     }
 }
